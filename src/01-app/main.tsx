c0385a9fbfee889fcb67415ee5e85e13d6988548
Alexey M <matteblack@MacBook-Air-Jeremy.local>
2025-06-11 21:10:24 +0300

    fix

--- src/01-app/main.tsx
+++ src/01-app/main.tsx
@@ -24,9 +24,8 @@ const router = createBrowserRouter([
    
     ],
   },
-]
+], {basename: import.meta.env.BASE_URL }
 );
-//, {basename: import.meta.env.BASE_URL }
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
